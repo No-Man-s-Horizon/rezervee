72b04a4d149156454fabaf45e79a38f87aacb97c
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-04-01 18:08:49 +0300

    Upd base

--- rezervee/settings.py
+++ rezervee/settings.py
@@ -123,16 +123,28 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'rezervee.wsgi.application'
+# WSGI_APPLICATION = 'rezervee.wsgi.application'
+WSGI_APPLICATION = 'rezervee.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<mydb>',
+        'USER': '<myuser>',
+        'PASSWORD': '<mypass>',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
